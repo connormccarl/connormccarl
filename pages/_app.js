46bb0aa3f781b1230f444f4fce0957c6249538ad
Connor McCarl <connormccarl@gmail.com>
2023-12-12 11:16:51 -0800

    added bootstrap

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import Nav from '@/components/Nav'
+// add bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css'
+// own css files
 import '@/styles/globals.css'
 
+import Nav from '@/components/Nav'
+import { useEffect } from 'react'
+
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    require("bootstrap/dist/js/bootstrap.bundle.min.js")
+  })
+  
   return (
     <>
       <Nav />
