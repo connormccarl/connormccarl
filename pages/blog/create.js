ac2ac97e0fc35498e3a2160819f257cfb7319816
Connor McCarl <connormccarl@gmail.com>
2023-12-13 09:47:43 -0800

    added form tags

--- pages/blog/create.js
+++ pages/blog/create.js
@@ -1,20 +1,75 @@
 import React from 'react'
+import { render } from 'react-dom'
+import { WithContext as ReactTags } from 'react-tag-input'
+
+const KeyCodes = {
+    comma: 188,
+    enter: 13
+}
+
+const delimiters = [KeyCodes.comma, KeyCodes.enter]
 
 export default function createPost() {
+    const [tags, setTags] = React.useState([
+        { id: 'Real Estate', text: 'Real Estate' }
+    ])
+
+    const handleDelete = i => {
+        setTags(tags.filter((tag, index) => index !== i))
+    }
+
+    const handleAdd = tag => {
+        setTags([...tags, tag])
+    }
+
+    const handleDrag = (tag, currPos, newPos) => {
+        const newTags = tags.slice()
+
+        newTags.splice(currPos, 1)
+        newTags.splice(newPos,0,tag)
+
+        // re-render
+        setTags(newTags)
+    }
+
+    const handleClick = index => {
+        console.log('The tag at index ' + index + ' was clicked')
+    }
+
   return (
-    <form>
+    <form class="mx-3">
         <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+            <label for="postTitle" class="form-label">Title</label>
+            <input type="text" class="form-control" id="postTitle" aria-describedby="titleHelp"/>
+            <div id="titleHelp" class="form-text">Make it something catchy</div>
         </div>
         <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1"/>
+            <label for="postDescription" class="form-label">Description</label>
+            <input type="text" class="form-control" id="postDescription" />
         </div>
-        <div class="mb-3 form-check">
-            <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
-            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+        <div class="mb-3">
+            <label for="postSlug" class="form-label">Slug</label>
+            <input type="text" class="form-control" id="postSlug" />
+        </div>
+        <div class="mb-3">
+            <ReactTags 
+                tags={tags}
+                delimiters={delimiters}
+                handleDelete={handleDelete}
+                handleAddition={handleAdd}
+                handleDrag={handleDrag}
+                handleTagClick={handleClick}
+                inputFieldPosition="bottom"
+                autocomplete
+            />
+        </div>
+        <div class="mb-3">
+            <label for="postContent" class="form-label">Content</label>
+            <textarea class="form-control" id="postContent" rows="3"></textarea>
+        </div>
+        <div class="mb-3">
+            <label for="postThumbnail" class="form-label">Thumbnail</label>
+            <input class="form-control" type="file" id="postThumbnail" />
         </div>
         <button type="submit" class="btn btn-primary">Submit</button>
     </form>
