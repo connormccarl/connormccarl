da04fa6ed712da46379307cfe3a19ab1bbe0b970
Connor McCarl <connormccarl@gmail.com>
2024-05-30 19:31:39 -0700

    changed to nextjs app router with typescript and tailwind

--- components/Button.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Button({ text }) {
-  return (
-    <button className='button' onClick={(event) => (event.target.innerText += '|')}>{text}</button>
-  )
-}
