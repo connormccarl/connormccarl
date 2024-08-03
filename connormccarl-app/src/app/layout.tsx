b450fde9201a5221ed3b125230f2949332e1ae6c
Connor McCarl <connormccarl@gmail.com>
2024-08-03 10:19:33 -0700

    added article: react concurrent mode

--- connormccarl-app/src/app/layout.tsx
+++ connormccarl-app/src/app/layout.tsx
@@ -30,6 +30,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="h-full antialiased" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
       <body className="flex h-full bg-zinc-50 dark:bg-black">
         <Providers>
           <div className="flex w-full">
